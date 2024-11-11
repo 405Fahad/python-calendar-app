77d8541d654aca3711c602c09688b29d226b48d3
405Fahad <fahadali121203@gmail.com>
2024-11-10 21:55:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,6 @@ def delete_event(event_id):
     event_manager.delete_event(event_id)
     flash('Event deleted!', 'success')
     return redirect(url_for('week_view'))
-with app.app_context():
-    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
